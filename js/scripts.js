0044948330f41dbfc7e8c4ebec5583c0b75e8f7b
Clara Munro <clarajmunro@gmail.com>
2019-01-25 10:02:22 -0800

    intialized set-up

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// business logic
+
+
+
+
+// user interface logic
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    var input = $("#input").val();
+    
+
+  });
+
+});
